d738ceeaf282ed406e0a89ee7b19627c64340414
Megan K Carroll <101686829+megtechnica@users.noreply.github.com>
2024-07-22 09:48:51 -0600

    Add files via upload
    images didn't upload

--- main.py
+++ main.py
@@ -1,41 +1,15 @@
-from data import question_data
 from question_model import Question
+from data import question_data
 from quiz_brain import QuizBrain
+from ui import QuizInterface
 
 question_bank = []
-
 for question in question_data:
-    question_text = question['text']
-    question_answer = question['answer']
+    question_text = question["question"]
+    question_answer = question["correct_answer"]
     new_question = Question(question_text, question_answer)
     question_bank.append(new_question)
 
+
 quiz = QuizBrain(question_bank)
-while quiz.still_has_questions():
-    quiz.next_question()
-
-print("You've completed the quiz!")
-
-class User:
-    def __init__(self, user_id, username):
-        self.id = user_id
-        self.username = username
-        self.followers = 0
-        self.following = 0
-
-    def follow(self, user):
-        user.followers += 1
-        self.following += 1
-
-
-
-user_1 = User()
-user_1.id = '001'
-user_1.username = 'angela'
-
-print(user_1.username)
-
-user_2 = User()
-user_2.id = '002'
-user_2.name = 'jack'
-
+quiz_ui = QuizInterface(quiz)
